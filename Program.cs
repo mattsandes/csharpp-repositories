13e89b0fd351c1269660a75ff18e3fd651c8cb45
Mateus Sandes <mateussandes@hotmail.com.br>
2024-10-01 07:37:03 -0300

    Praticando variavies e leitura de dados

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿// using System; Não e necessário usar isso pois
 //o name space ja esta importado nesse projeto;
 
+using System.Globalization;
+
 namespace CsharpProject //nome do projeto;
 {
     internal class Program
@@ -8,6 +10,27 @@ namespace CsharpProject //nome do projeto;
         static void Main(string[] args)
         {
             Console.WriteLine("Hello world");
+
+            Console.Write("Hello");
+            Console.Write("Wold");
+
+            //lendo dados do usuário;
+
+            var dadoDeEntrada = Console.ReadLine();
+
+            Console.WriteLine(dadoDeEntrada);
+
+            double valorDecimal = 12.890;
+            double outroValorDecimal = 47.555;
+
+            //imprimindo 3 casas depois da virgula;
+            Console.WriteLine(valorDecimal.ToString("F3"));
+
+            //imprimindo virgula ao invez de ponto;
+            Console.WriteLine(valorDecimal.ToString("F3", CultureInfo.InvariantCulture));
+
+            Console.WriteLine("Valor 1: " + valorDecimal + 
+            "\nOutro valor decimal: " + outroValorDecimal);
         }
     }
 }
